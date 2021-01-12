487c5e91459f5ca9c22dd05d2c5b818e509266da
Pingkan Purwanto <pingpurwanto@gmail.com>
2021-01-12 15:38:25 +0700

    Updated main.js

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = "Hello world!"
+
+let myVariable;
+myVariable= 'Bob';
+myVariable;
+let myVariable = 'Bob';
+myVariable = 'Steve';
